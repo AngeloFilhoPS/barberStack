f4fffc389d0313e16596f9c723c7dae2a42e3660
Angelo <ampsf@icomp.ufam.edu.br>
2021-01-28 08:32:07 -0400

    feat: password with cryptography, session service

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,17 @@
 import { Router } from 'express';
 
 import appointmentRouter from './appointments.routes';
+import sessionsRouter from './sessions.routes';
 import usersRouter from './users.routes';
+
 
 const routes = Router();
 
 
 routes.use('/appointments', appointmentRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+
 
 
 export default routes
